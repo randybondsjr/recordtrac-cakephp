a4468840491e8ef51f9a7a2f0d625da35ef361f3
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-06-19 16:24:19 -0700

    Added about view

--- app/Controller/RecordTracController.php
+++ app/Controller/RecordTracController.php
@@ -1,5 +1,12 @@
 <?php
 class RecordTracController extends AppController {
+  /**
+  * This controller does not use a model
+  *
+  * @var array
+  */
+	public $uses = array();
+	
   public function index() {
     
   }
