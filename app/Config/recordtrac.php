602955d2fd0144b4e720a8abcddf5c7920d3af47
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-06-20 09:42:17 -0700

    Adding App Config Constants

---
+++ app/Config/recordtrac.php
@@ -0,0 +1,12 @@
+<?php 
+/* Configuration File for CakePHP RecordTrac Port */
+$config = array(
+    'Agency' => array(
+      'name' => 'City of Yakima',
+      'tagline' => 'The Heart of Central Washington',
+      'url' => 'http://www.yakimawa.gov'
+    ),
+    'App' => array(
+      'url' => 'http://recordtrac'
+    )
+);
