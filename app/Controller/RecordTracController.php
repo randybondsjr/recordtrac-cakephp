a61607a161bc4448ddaac03916af840009c99082
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-07-25 10:53:45 -0700

    Formatting cleanup

--- app/Controller/RecordTracController.php
+++ app/Controller/RecordTracController.php
@@ -1,12 +1,6 @@
 <?php
 class RecordTracController extends AppController {
-  /**
-  * This controller does not use a model
-  *
-  * @var array
-  */
-	public $uses = array();
-	
+	public $uses = array();//no model used!
   public function index() {
     $this->loadModel('Requests');
     $this->set('totalRequests', $this->Requests->find('count'));
