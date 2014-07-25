4dc2432699386c2263b9d956bf45d1c4f39dad3b
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-07-25 11:26:52 -0700

    Added addUser logic

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -13,4 +13,17 @@ class AdminController extends AppController {
   public function index() {
 
   }
+  
+  public function addUser() {
+      if ($this->request->is('post')) {
+          $this->User->create();
+          if ($this->User->save($this->request->data)) {
+              $this->Session->setFlash(__('The user has been saved'));
+              return $this->redirect(array('action' => 'index'));
+          }
+          $this->Session->setFlash(
+              __('The user could not be saved. Please, try again.')
+          );
+      }
+  }
 }
