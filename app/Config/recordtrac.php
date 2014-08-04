2e199e05ea079b18b09583a8bb3a339ccf8fc48c
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-08-04 16:17:10 -0700

    removed unneeded config option, added 2

--- app/Config/recordtrac.php
+++ app/Config/recordtrac.php
@@ -4,9 +4,7 @@ $config = array(
     'Agency' => array(
       'name' => 'City of Yakima',
       'tagline' => 'The Heart of Central Washington',
-      'url' => 'http://www.yakimawa.gov'
-    ),
-    'App' => array(
-      'url' => 'http://recordtrac'
+      'url' => 'http://www.yakimawa.gov',
+      'responseDays' => '5'
     )
 );
