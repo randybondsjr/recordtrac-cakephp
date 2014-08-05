2ab71060834486502b0881ad8248118490af3c1c
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-08-05 16:36:31 -0700

    Changed to when document is ready

--- app/webroot/js/view-request.js
+++ app/webroot/js/view-request.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
   $('.lead').readmore({
     moreLink: '<p class="lead"><a href="#">More&hellip;</a></p>',
     lessLink: '<p class="lead"><a href="#">Less&hellip;</a></p>'
