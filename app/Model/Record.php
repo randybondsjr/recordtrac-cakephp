8261fda28fd06671acae3612bf5f9d923e1ccfc6
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-08-21 11:18:06 -0700

    validation for file upload

--- app/Model/Record.php
+++ app/Model/Record.php
@@ -1,20 +1,54 @@
 <?php 
 class Record extends AppModel {
+
+  public $actsAs = array(
+    'Upload.Upload' => array(
+        'filename',
+        'thumbnailMethod' => 'php'
+    )
+    
+  );
  	public $validate = array(
-    'department_id' => array(
+    'description' => array(
       'rule' => 'notEmpty'
     ),
-    'text' => array(
-      'rule1' => array(
-        'rule'    => 'notEmpty',
-        'last'    => false
-       ),
-      'rule2' => array(
-          'rule'    => array('minLength', 50),
-          'message' => 'Please provide more details so that we may complete your request quickly.'
-      )
+    'filename' => array(
+        'rule' => array(
+          'isValidMimeType', 
+          array('application/pdf', 
+                'image/png', 
+                'application/postscript', 
+                'application/epub+zip', 
+                'application/msword', 
+                'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+                'application/vnd.ms-powerpoint',
+                'application/vnd.ms-powerpoint',
+                'application/vnd.ms-excel',
+                'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'text/plain',
+                'application/rtf',
+                'image/jpeg',
+                'image/gif'
+               ), 
+          false
+        ),
+        'message' => '<p>File type is not allowed</p>
+                      <p>Allowed File Types:</p>
+                      <ul>
+                      <li>Adobe PDF (.pdf)</li>
+                      <li>Adobe PostScript (.ps)</li>
+                      <li>Electronic Publication (.epub)</li>
+                      <li>Microsoft Word (.doc/ .docx)</li>
+                      <li>Microsoft PowerPoint (.ppt/.pps/.pptx)</li>
+                      <li>Microsoft Excel (.xls/.xlsx)</li>
+                      <li>Plain text (.txt)</li>
+                      <li>Rich text format (.rtf)</li>
+                      <li>Image Formats (.png/.jpg/.gif)</li>
+                      </ul>
+                     ',
+        'allowEmpty' => true
     )
-
   );
-	
+
 }
