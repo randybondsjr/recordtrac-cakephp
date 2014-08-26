c054c67aad8f90e1157ba0123475d32f844b1086
Randy Bonds Jr <randy.bonds@yakimawa.gov>
2014-08-26 15:53:08 -0700

    Visualization for avg response days

--- app/Controller/RecordTracController.php
+++ app/Controller/RecordTracController.php
@@ -2,7 +2,45 @@
 class RecordTracController extends AppController {
 	public $uses = array();//no model used!
 	public $components = array('HighCharts.HighCharts');
+	private function get_avg_response_time($deptID = null){
+  	$response_time = 0;
+  	$num_closed = 0;
+  	$this->loadModel('Request');
+    $requests = $this->Request->find('all', array('conditions' => array('Request.department_id' => $deptID)));
+
+    foreach($requests as $request){
+      if($request["Request"]["status_id"] == 2){ //only do this if it's closed
+        $response_time = $response_time + strtotime($request["Request"]["status_updated"]) - strtotime($request["Request"]["created"]);
+        $num_closed = $num_closed + 1;
+      }
+    }
+    if($num_closed > 0){
+       $avg = $response_time / $num_closed;
+       return $avg;
+    }else {
+      return 0;
+    }
+	}
+  
+  private function responseSort($a,$b){
+    return $a['avg_response_time'] - $b['avg_response_time'];
+  }
+  
+  private function secondsToDays($seconds){
+    $ret = "";
+
+    /*** get the days ***/
+    $days = intval(intval($seconds) / (3600*24));
+    if($days> 0)
+    {
+        $ret .= $days;
+    }
+
+    return $ret;
+  }
+  
   public function index() {
+    //top requests
     $this->loadModel('Requests');
     $this->set('totalRequests', $this->Requests->find('count'));
     $top5depts = $this->Requests->query('
@@ -20,11 +58,36 @@ class RecordTracController extends AppController {
       $requests[] = intval($dept[0]["totalRequest"]);
       $depts[] = $dept["d"]["name"];
     }
+    
+        
+    //AVG RESPONSE TIME
+    $this->loadModel('Departments');
+    $departments = $this->Departments->find('all');
+    $avgResponseTime = array();
+    $i = 0;
+    foreach($departments as $department){
+      $avgResponseTime[$i]["name"] = $department["Departments"]["name"];
+      $avgResponseTime[$i]["avg_response_time"] = $this->get_avg_response_time($department["Departments"]["id"]);
+      $i++;
+    }
+    usort($avgResponseTime, array($this,'responseSort'));
 
+    $avgNames = array();
+    $avgTimes = array();
+    $i = 0;
+    foreach ($avgResponseTime as $avg){
+      if($avg["avg_response_time"] != 0){
+        if($i != 5){
+          $avgNames[$i] = $avg["name"];
+          $avgTimes[$i] = intval($this->secondsToDays($avg["avg_response_time"]));
+          $i++;
+        }
+      }
+    }
+    
+    //top requests chart
     $chartName = 'Top Requests';
-    
     $mychart = $this->HighCharts->create( $chartName, 'column' );
-    
     $this->HighCharts->setChartParams(
             $chartName,
             array(
@@ -39,76 +102,36 @@ class RecordTracController extends AppController {
                   'chartAlignTicks'			=> FALSE,
                   'chartTheme'                => 'skies',
                   'chartBackgroundColor' => 'rgba(255, 255, 255, 0.1)',
-                  //'chartBackgroundColorLinearGradient' 	=> array(0,0,0,300),
-                  //'chartBackgroundColorStops'		=> array(array(0,'rgb(217, 217, 217)'),array(1,'rgb(255, 255, 255)')),
-  
                   'title'					=> 'Requests (Top 5 Departments)',
                   'titleAlign'				=> 'left',
                   'titleFloating'				=> TRUE,
                   'titleX'				=> 20,
                   'titleY'				=> 20,
-  
                   'legendEnabled' 			=> FALSE,
-  
                   'tooltipEnabled' 			=> FALSE,
-                  // 'tooltipBackgroundColorLinearGradient' => array(0,0,0,50),   // triggers js error
-                  // 'tooltipBackgroundColorStops' => array(array(0,'rgb(217, 217, 217)'),array(1,'rgb(255, 255, 255)')),
-  
-                  //'plotOptionsLinePointStart' 		=> strtotime('-30 day') * 1000,
-                  //'plotOptionsLinePointInterval' 	=> 24 * 3600 * 1000,
-  
-                  //'xAxisType' 				=> 'datetime',
-                  //'xAxisTickInterval' 			=> 10,
-                  //'xAxisStartOnTick' 			=> TRUE,
-                  //'xAxisTickmarkPlacement' 		=> 'on',
-                  //'xAxisTickLength' 			=> 10,
-                  //'xAxisMinorTickLength' 		=> 5,
-  
                   'xAxisLabelsEnabled' 			=> TRUE,
                   'xAxisLabelsAlign' 			=> 'center',
                   'xAxisLabelsStep' 			=> 0,
-                  //'xAxisLabelsRotation' 		=> -35,
                   'xAxislabelsX' 				=> 5,
                   'xAxisLabelsY' 				=> 20,
                   'xAxisCategories'          		=> $depts,
-  
-                  //'yAxisMin' 				=> 0,
-                  //'yAxisMaxPadding'			=> 0.2,
-                  //'yAxisEndOnTick'			=> FALSE,
-                  //'yAxisMinorGridLineWidth' 		=> 0,
-                  //'yAxisMinorTickInterval' 		=> 'auto',
-                  //'yAxisMinorTickLength' 		=> 1,
-                  //'yAxisTickLength'			=> 2,
-                  //'yAxisMinorTickWidth'			=> 1,
-  
                   'yAxisTitleText' 			=> 'Requests',
-                  //'yAxisTitleAlign' 			=> 'high',
-                  //'yAxisTitleStyleFont' 		=> '14px Metrophobic, Arial, sans-serif',
-                  //'yAxisTitleRotation' 			=> 0,
-                  //'yAxisTitleX' 			=> 0,
-                  //'yAxisTitleY' 			=> -10,
-                  //'yAxisPlotLines' 			=> array( array('color' => '#808080', 'width' => 1, 'value' => 0 )),
-  
-                  // autostep options
                   'enableAutoStep' 			=> FALSE
             )
     );
     
     $series = $this->HighCharts->addChartSeries();
-    
-    $series->addData($requests);
-    
+    $series->addName('Requests')
+           ->addData($requests);
     $mychart->addSeries($series);
     
+    //avg response time chart
     $chartName = 'Avg Response Time';
-    
     $mychart = $this->HighCharts->create( $chartName, 'bar' );
-
     $this->HighCharts->setChartParams(
             $chartName,
             array(
                   'renderTo'				=> 'daystorespond',  // div to display chart inside
-                  
                   'chartMarginTop' 			=> 60,
                   'chartMarginLeft'			=> 20,
                   'chartMarginRight'			=> 30,
@@ -119,63 +142,28 @@ class RecordTracController extends AppController {
                   'chartAlignTicks'			=> FALSE,
                   'chartTheme'                => 'skies',
                   'chartBackgroundColor' => 'rgba(255, 255, 255, 0.1)',
-                  
                   'title'					=> 'Average # Days to Respond (quickest 5 departments)',
                   'titleAlign'				=> 'left',
                   'titleFloating'				=> TRUE,
                   'titleX'				=> 20,
                   'titleY'				=> 20,
-                  
                   'legendEnabled' 			=> FALSE,
                   'tooltipEnabled' 			=> FALSE,
-                  // 'tooltipBackgroundColorLinearGradient' => array(0,0,0,50),   // triggers js error
-                  // 'tooltipBackgroundColorStops' => array(array(0,'rgb(217, 217, 217)'),array(1,'rgb(255, 255, 255)')),
-                  
-                  //'plotOptionsLinePointStart' 		=> strtotime('-30 day') * 1000,
-                  //'plotOptionsLinePointInterval' 	=> 24 * 3600 * 1000,
-                  
-                  //'xAxisType' 				=> 'datetime',
-                  //'xAxisTickInterval' 			=> 1,
-                  //'xAxisStartOnTick' 			=> TRUE,
-                  //'xAxisTickmarkPlacement' 		=> 'on',
                   'xAxisTickLength' 			=> 0,
-                  //'xAxisMinorTickLength' 		=> 5,
-                  
                   'xAxisLabelsEnabled' 			=> TRUE,
                   'xAxisLabelsAlign' 			=> 'left',
                   'xAxisLabelsStep' 			=> 0,
-                  //'xAxisLabelsRotation' 		=> -35,
                   'xAxislabelsX' 				=> 5,
                   'xAxisLabelsY' 				=> 5,
-                  'xAxisCategories'     => $depts,
-                  
-                  //'yAxisMin' 				=> 0,
-                  //'yAxisMaxPadding'			=> 0.2,
-                  //'yAxisEndOnTick'			=> FALSE,
-                  //'yAxisMinorGridLineWidth' 		=> 0,
-                  //'yAxisMinorTickInterval' 		=> 'auto',
-                  //'yAxisMinorTickLength' 		=> 1,
-                  //'yAxisTickLength'			=> 2,
-                  //'yAxisMinorTickWidth'			=> 1,
-                  
-                  
+                  'xAxisCategories'     => $avgNames,
                   'yAxisTitleText' 		=> 'Days',
-                  //'yAxisTitleAlign' 		=> 'high',
-                  //'yAxisTitleStyleFont' 	=> '14px Metrophobic, Arial, sans-serif',
-                  //'yAxisTitleRotation' 		=> 0,
-                  //'yAxisTitleX' 		=> 0,
-                  //'yAxisTitleY' 		=> -10,
-                  //'yAxisPlotLines' 		=> array( array('color' => '#808080', 'width' => 1, 'value' => 0 )),
-                  
-                  // autostep options
                   'enableAutoStep' 		=> FALSE
           )
     );
 
     $series = $this->HighCharts->addChartSeries();
-
-    $series->addData($requests);
-
+    $series->addName('Days')
+           ->addData($avgTimes);
     $mychart->addSeries($series);
 
 
